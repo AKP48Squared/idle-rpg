dfc24212c4e5e51ed1c86dafc2f8fb815ada241f
feildmaster <feildmaster@users.noreply.github.com>
2016-05-14 18:48:31 -0500

    List channel configuration for now Setting will have to come later

--- commands/options.js
+++ commands/options.js
@@ -4,10 +4,34 @@ function Command() {
   this.perms = "irc.channel.op";
 }
 
+function build(object) {
+  var str = [];
+  Object.keys(object).forEach(function (key) {
+    var val = object[key];
+    if (val === true) {
+      val = "enabled";
+    } else if (val === false) {
+      val = "disabled";
+    }
+    str.push(`${key}-${val}`);
+  });
+  return str.join(", ");
+}
+
 Command.prototype.process = function(context) {
   // Make sure there's a channel
-  var options = context.irpg.getChannelOptions(context.irpgChannel);
+  var options = {};
+  var chan = context.irpg.getChannelOptions(context.irpgChannel);
+  Object.keys(chan).forEach(function (key) {
+    if (key !== "enabled") options[key] = chan[key];
+  });
   // By default list the current options
+  if (context.irpgText.length === 0) {
+    return context.reply(`Options: ${build(options)}`);
+  }
+  var args = context.irpgText.split(" ");
+  context.reply("To be implemented");
+  // If option and no value, explain the option
   // Otherwise change the option to the provided value
 };
 
